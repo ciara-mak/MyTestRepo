a5bc80d79ef45a373b1f1652d9fb278075b68501
ciara-mak <makievskaya.ciara@gmail.com>
2022-08-22 11:04:56 +0300

    fixed bug

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,6 +6,9 @@ else
     echo -e "$dir is not a directory!\n"
     exit 101
 fi
-
-top1=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 2)
-disk=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 1)
+echo "The heaviest subfolder of $dir is:"
+top1=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 2 | \
+	 tail -n 1| cut -f 2)
+disk=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 2 | \
+	 tail -n 1| cut -f 1)
+echo -e "$top1 with disk usage of $disk"
